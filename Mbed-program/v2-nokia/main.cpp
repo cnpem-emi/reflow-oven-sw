84c3ae664fbc190e355b4009c7c4668512145910
Guilherme Ruyz Gutierrez <guilherme.gutierrez@cnpem.br>
2022-08-09 12:21:43 -0300

    v2-nokia - program to read temperature

--- Mbed-program/v2-nokia/main.cpp
+++ Mbed-program/v2-nokia/main.cpp
@@ -3,13 +3,20 @@
 #include <SPI.h> 
 #include <Adafruit_GFX.h>  
 #include <Adafruit_PCD8544.h> 
-#include <SPI.h>
 #include "NOKIA_5110.h"
+#include "max6675.h"
 
-// main() runs in its own thread in the OS
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// pin definition
 float variable; 
 DigitalIn pb(p14); //push botton
+SPI spi(p5,p6,p7);
+max6675 max_spi(spi,p18);
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+float temp;
 const char arrow[5] = {0x41, 0x22, 0x14, 0x08, 0x00};
 
 int main()
@@ -32,19 +39,16 @@ int main()
     variable = 0;
  
     // Draw a test pattern on the LCD and stall for 15 seconds
-    myLcd.DrawString("my friend");
-    for(int i = 0; i < 10; i++) {
-    snprintf(test, 16, "test %d", i);
+    while (1) {
+    float temp = max_spi.read_temp();
+    snprintf(test, 16, "test %.2f", temp);
     myLcd.SetXY(0,0);
     myLcd.DrawString(test);
-    for(int j = 0; j < 6;j++){
-        myLcd.SendDrawData(arrow[j]);
-    }
+    //for(int j = 0; j < 6;j++){
+    //    myLcd.SendDrawData(arrow[j]);
+    //}
     wait_us( 2000000 );
-    }
-    
+    }   
     // Turn off the LCD and enter an endless loop
-    while (1){
-    }
 }
        
