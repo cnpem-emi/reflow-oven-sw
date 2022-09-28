b44a936b150996a37d20409432c3dd6bbf48aa8d
Guilherme Ruyz Gutierrez <guilherme.gutierrez@cnpem.br>
2022-09-28 08:53:32 -0300

    Version to test PID variables

--- version-mbed-program/pid-v0.2.0/test v0.1.0/pid-v0.1.0.cpp
+++ version-mbed-program/pid-v0.2.0/test v0.1.0/pid-v0.1.0.cpp
@@ -4,7 +4,6 @@
 #include "mbed.h"
 #include <SPI.h>
 #include <string>
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // pins definition
@@ -22,9 +21,7 @@ DigitalIn pb3_right(p30);        // push botton
 SPI spi(p5, p6, p7);             // spi communication to temperature sensor
 max6675 max_spi(spi, p18);       // max6675 pin definitions
 BufferedSerial pc(USBTX, USBRX); // Serial communication
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 // variables definition
 float temp;
 int variable = 1;
@@ -37,72 +34,27 @@ float temp_o;     // old aquisition temperature
 float temp_d;     // temperature variation
 float temp_error; // variable to PID
 float temp_set;   // set temp variable to PID
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 // controller variables
-const float pwm_period = 1.0f / 0.60f;
+const float pwm_period = 1/0.6;
 float PID_value;
-float PID_p;                                // proportional
-float PID_i;                                // integral
-float PID_d;                                // derivated
+float PID_p;                          // proportional
+float PID_i;                          // integral
+float PID_d;                          // derivated
 PID controller(0.2, 50.0, 0.4, pwm_period); // variables PID controller
 float set_pid;
 int change_info;
-bool weld_profile;
-int step;
-
-// Function paramters
-float a;float b;float t;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // characters to write lcd
 const char arrow[5] = {0x41, 0x22, 0x14, 0x08, 0x00}; // simbole: >
+void auto_control1(void);
 void auto_control(void);
 
-void Set_temperature() {
-  if (weld_profile == true){
-    set_pid = a * t/0.6 + b;
-    t++;
-  }
-  if (weld_profile == false){
-      t++;
-  }
-  controller.setSetPoint(set_pid);
-}
+void extracted() { return; }
 
-void Set_temperature_perfil() {
-  if ((step == 1) && (weld_profile = true)) {
-    a = 12.5f / 15.0f;
-    b = 25.0f;
-    t = 0;
-    step = 2;
-  }
-  if ((step == 2) && (weld_profile == true) && (t >= 90)) {
-    a = 5.0 / 9.0f;
-    b = 150.0f;
-    t = 0;
-    step = 3;
-  }
-  if ((step == 3) && (weld_profile == true) && (t >= 54)) {
-    a = 3.0/6.0;
-    b = 200;
-    step = 4;
-  }
-    if ((step == 4) && (weld_profile == true) && (t >= 54)) {
-    weld_profile = false;
-    set_pid = 230.0;
-    step = 5; 
-  }
-    if ((step == 5) && (weld_profile == false) && (t >= 36)) {
-    }
-  // if ((weld_profile == false) && (t >= 36)){}
-    Set_temperature();
-
-}
-
-void weld_profile_sn_pb() {
+int read_temperature(int a) {
   char *test = (char *)malloc(20);
   char *msg_pc = (char *)calloc(13, 1);
   char *buff = (char *)malloc(3);
@@ -114,8 +66,8 @@ void weld_profile_sn_pb() {
   controller.setInputLimits(0.0, 360.0);
   controller.setOutputLimits(0.00, 1.00);
   controller.setMode(1);
+  controller.setSetPoint(230.0);
   controller.setTunings(0.2, 50.0, 0.4);
-
   // Init the data structures and NokiaLcd class
   LcdPins myPins;
   myPins.sce = p8;
@@ -124,17 +76,15 @@ void weld_profile_sn_pb() {
   myPins.mosi = p11;
   myPins.miso = NC;
   myPins.sclk = p13;
-
   // Start the LCD
   NokiaLcd myLcd(myPins);
   myLcd.InitLcd();
   myLcd.ClearLcdMem();
 
-  step = 1;
   variable = 0;
-  led2 = 0;
+  led2 = 0; 
 
-  // pc.write("To view Temperature - Press key: 1", 36);
+  //pc.write("To view Temperature - Press key: 1", 36);
 
   // Interrupt to  pwm adjuste
   pwm2_out.fall(auto_control);
@@ -157,14 +107,13 @@ void weld_profile_sn_pb() {
     }
 
     if (change_info == 1) {
-      led2 = 1;
+      led2 =  1; 
       temp_n = max_spi.read_temp();
       temp_d = temp_n - temp_o;
       if (temp_d < 0) {
         temp_d = -temp_d;
       }
       if ((temp_d <= 5) && (temp_n != 0)) {
-        Set_temperature_perfil();
         controller.setProcessValue(temp_n);
         pwm_value = controller.compute();
         pwm1.write(pwm_value);
@@ -172,22 +121,23 @@ void weld_profile_sn_pb() {
           snprintf(msg_pc, 13, "%03.2f;%01.2f", temp_n, pwm_value);
           wait_us(10000);
           pc.write(msg_pc, 13);
+
         }
 
         if (buff[0] == '1') {
-          snprintf(msg_pc, 13, "\nTemp.: %03.2f", temp_n);
-          pc.write(msg_pc, 13);
-          wait_us(10000);
-          snprintf(msg_pc, 12, "\nPWM: %01.2f", pwm_value);
-          pc.write(msg_pc, 12);
-          wait_us(10000);
-        }
-        led2 = 0;
+            snprintf(msg_pc, 13, "\nTemp.: %03.2f", temp_n);
+            pc.write(msg_pc, 13);
+            wait_us(10000);
+            snprintf(msg_pc, 12, "\nPWM: %01.2f", pwm_value);
+            pc.write(msg_pc, 12);
+            wait_us(10000);
+      }
+      led2 =  0; 
       }
       temp_o = temp_n;
       change_info = 0;
     }
-    pc.sync();
+  pc.sync();
   }
   // for(int j = 0; j < 6;j++){
   //     myLcd.SendDrawData(arrow[j]);
@@ -195,10 +145,8 @@ void weld_profile_sn_pb() {
   // pc.write ("e\n", 3);
 }
 
-void auto_control(void) { change_info = 1; }
-
-void option_menu() {
-
+void auto_control(void) {
+  change_info = 1;
 }
 
 int main() {
@@ -213,17 +161,15 @@ int main() {
   myPins.mosi = p11;
   myPins.miso = NC;
   myPins.sclk = p13;
-
   NokiaLcd myLcd(myPins);
   myLcd.InitLcd();
   myLcd.ClearLcdMem();
-
   // pwm start in 0%
   pwm1.write(0);
   pwm1.period(pwm_period);
   pwm2.period(pwm_period);
   pwm2.write(0.75);
-  led3.period(pwm_period);
+  led3.period(1.67f);
   led3.write(0.75);
 
   pwm_value = 0;
@@ -237,8 +183,7 @@ int main() {
     myLcd.SetXY(0, 0);
     myLcd.DrawString(test);
     while (!pb1_left) {
-      weld_profile = true;
     }
-    weld_profile_sn_pb();
+    temp_function = read_temperature(1);
   }
 }
